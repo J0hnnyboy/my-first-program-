824541536a301fff8f38a11209f2f212883e8358
J0hnnyboy <101340085+J0hnnyboy@users.noreply.github.com>
2022-09-07 14:10:56 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Goodbye")
+})
+basic.forever(function () {
+	
+})
